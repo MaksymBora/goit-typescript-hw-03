4147d2305c3a0c49428e8edcefc93d18e7b3df81
MaksymBora <maxboraod@gmail.com>
2023-10-06 09:45:45 +0200

    complete

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,49 @@
+class Key {
+  private signature: number = Math.random();
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean = false;
+  public key: Key;
+  public tenants: Person[] = [];
+
+  abstract comeIn(person: Person): void;
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door && this.key === person.getKey()) {
+      this.tenants.push(person);
+    }
+  }
+
+  openDoor(key: Key): void {
+    if (this.key === key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
